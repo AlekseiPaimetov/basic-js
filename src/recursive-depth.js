ac4a654a490974961d7963dc57220c091fc57193
Aleksei <Aleksei_Paimetov@epam.com>
2020-07-02 00:40:22 +0400

    Added solution for extended-repeater task. Solution for recursive-depth doesn't sucess passed test for number of recursions calls.

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor () {
+    this.depth = 1;
+    this.currentDepth = 1;
   }
+
+  calculateDepth(arr) {
+    if (!Array.isArray(arr)){
+      return 0;
+    }
+
+    for (let i = 0; i < arr.length; i++ ) {
+      if (Array.isArray(arr[i])){
+        this.currentDepth++;
+        this.calculateDepth(arr[i]);
+        if (this.depth < this.currentDepth) {
+          this.depth = this.currentDepth;
+        }
+        this.currentDepth = 1;
+      }
+    }
+    
+    return this.depth;
+  }
+
 };
